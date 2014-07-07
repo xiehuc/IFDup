d1e9ca0953b946f3d1e3aca2fd0e7f84960cde1f
xiehuc <xiehuc@gmail.com>
2014-07-07 12:59:21 +0800

    establish framework of LockInst
    Signed-off-by: xiehuc <xiehuc@gmail.com>

---
+++ lib/LockInst.h
@@ -0,0 +1,20 @@
+#ifndef _LOCK_INST_H_H
+#define _LOCK_INST_H_H
+#include <llvm/Pass.h>
+#include <llvm/IR/Instruction.h>
+
+class Lock:public llvm::ModulePass
+{
+   public:
+	static char ID;
+	Lock():ModulePass(ID){}
+	void getAnalysisUsage(llvm::AnalysisUsage& AU) const
+	{
+	    AU.setPreservesAll();
+	}
+	bool runOnModule(llvm::Module& M);
+	void lock_inst(llvm::Instruction* I);
+};
+
+
+#endif
