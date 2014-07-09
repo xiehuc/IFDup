f9cd3e7c5c6aa342e12b3983cf81df1cd2d031ae
HaoMengHIT <haomeng1@yeah.net>
2014-07-09 22:48:24 +0800

    Lock and unlock LoadInst

--- lib/LockInst.cpp
+++ lib/LockInst.cpp
@@ -5,6 +5,7 @@
 #include <llvm/IR/Constants.h>
 #include <llvm/IRReader/IRReader.h>
 #include <llvm/Support/SourceMgr.h>
+#include <llvm/IR/LLVMContext.h>
 #include <sstream>
 #include <iostream>
 #include <list>
@@ -12,54 +13,59 @@ using namespace std;
 using namespace llvm;
 
 char Lock::ID=0;
-static RegisterPass<Lock> X("Lock","Lock and Unlock the instructions");
+char Unlock::ID=0;
+static RegisterPass<Lock> X("Lock","Lock the instructions");
+static RegisterPass<Unlock> Y("Unlock","Unlock the locked instructions");
+
+//Convert int to string
 string getString(int tmp)
 {
-    stringstream newstr;
-    newstr<<tmp;
-    return newstr.str();
+   stringstream newstr;
+   newstr<<tmp;
+   return newstr.str();
 }
+
+//Judge the Type
 string judgeType(Type* ty)
 {
-    string name="";
-    unsigned tyid=ty->getTypeID();
-
-    if(tyid==14)
-    {
-       Type* tmp=ty;
-       while(tyid == 14)
-       {
-          tmp=tmp->getPointerElementType();
-          tyid=tmp->getTypeID();
-          name+="p";
-       }
-       if(tyid==10)
-          name=name+getString(tyid)+getString(tmp->getPrimitiveSizeInBits());
-    }
-    else if(tyid==10)
-    {
-       name=name+getString(tyid)+getString(ty->getPrimitiveSizeInBits());
-    }
-    else
-       name=getString(tyid);
-    return name;
+   string name="";
+   unsigned tyid=ty->getTypeID();
+   if(tyid==14){
+      Type* tmp=ty;
+      while(tyid == 14){
+         tmp=tmp->getPointerElementType();
+         tyid=tmp->getTypeID();
+         name+="p";
+      }
+      if(tyid==10)
+         name=name+getString(tyid)+getString(tmp->getPrimitiveSizeInBits());
+   }
+   else if(tyid==10){
+      name=name+getString(tyid)+getString(ty->getPrimitiveSizeInBits());
+   }
+   else
+      name=getString(tyid);
+   return name;
 }
+
+//Get the name of function in  CallInst
 string getFuncName(Instruction* I,SmallVector<Type*,8>& opty)
 {
-    string funcname="";
-    unsigned size=opty.size();
-    if(size>0)
-       funcname+=judgeType(I->getType())+".";
-    else
-       funcname+=judgeType(I->getType());
-    unsigned i;
-    for(i=0;i < size-1;i++)
-    {
-        funcname+=(judgeType(opty[i])+".");
-    }
-    funcname+=judgeType(opty[i]);
-    return funcname;
+   string funcname="";
+   unsigned size=opty.size();
+   if(size>0)
+      funcname+=judgeType(I->getType())+".";
+   else
+      funcname+=judgeType(I->getType());
+   unsigned i;
+   for(i=0;i < size-1;i++){
+      funcname+=(judgeType(opty[i])+".");
+   }
+   funcname+=judgeType(opty[i]);
+   return funcname;
 }
+
+//Lock instructions
 void Lock::lock_inst(Instruction *I)
 {
    LLVMContext& C = I->getContext();
@@ -74,28 +80,28 @@ void Lock::lock_inst(Instruction *I)
    Instruction* T = NULL;
    MDNode* LockMD = MDNode::get(C, MDString::get(C, "IFDup"));
    string nametmp=getFuncName(I,OpTypes);
-   if (LoadInst* LI=dyn_cast<LoadInst>(I))
-   {
+   if (LoadInst* LI=dyn_cast<LoadInst>(I)){
       Constant* Func = M->getOrInsertFunction("lock.load."+nametmp, FT);
       CallInst* CI = CallInst::Create(Func, OpArgs, "", I);
 
       unsigned align = LI->getAlignment();
       CI->setMetadata("align."+getString(align), LockMD);
       if(LI->isAtomic())
-         CI->setMetadata("atomic", LockMD);
+         CI->setMetadata("atomic."+getString(LI->getOrdering()), LockMD);
       if(LI->isVolatile())
          CI->setMetadata("volatile", LockMD);
       I->replaceAllUsesWith(CI);
+      for(unsigned i =0;i < I->getNumOperands();i++)
+      {
+         I->setOperand(i, UndefValue::get(I->getOperand(i)->getType()));
+      }
+      I->removeFromParent();
       T = CI;
-
+   }
+   else if(StoreInst* SI=dyn_cast<StoreInst>(I)){
 
    }
-   else if(StoreInst* SI=dyn_cast<StoreInst>(I))
-   {
-
-   }
-   else if(CmpInst* CI=dyn_cast<CmpInst>(I))
-   {
+   else if(CmpInst* CI=dyn_cast<CmpInst>(I)){
 
    }
    else if(BinaryOperator* BI=dyn_cast<BinaryOperator>(I)){
@@ -108,38 +114,123 @@ void Lock::lock_inst(Instruction *I)
    }
 }
 
+
 #include <llvm/Support/InstIterator.h>
 bool Lock::runOnModule(Module &M)
 {
    for(Module::iterator F = M.begin(), FE = M.end(); F!=FE; ++F){
-      for(inst_iterator I = inst_begin(F), IE = inst_end(F); I!=IE; ++I){
-         if(isa<LoadInst>(&*I))
-            lock_inst(&*I);
+      inst_iterator I = inst_begin(F);
+      while(I!=inst_end(F)){
+         Instruction* self = &*I;
+         I++;
+         if(isa<LoadInst>(self))
+            lock_inst(self);
+      }
+   }
+   return false;
+}
+bool Unlock::runOnModule(Module &M)
+{
+   for(Module::iterator F = M.begin(), FE = M.end(); F!=FE; ++F){
+      inst_iterator I = inst_begin(F);
+      while(I!=inst_end(F)){
+         Instruction* self = &*I;
+         I++;
+         if(isa<CallInst>(self)){
+            unlock_inst(self);
+         }
       }
    }
    return false;
 }
 
-/*
-void Lock::unlock_inst(Instruction *I)
+SmallVector<string, 10> str_split(string str,string pattern)
 {
-    if (CallInst* CI=dyn_cast<CallInst>(I))
-    {
-	Function* tmp=CI->getCalledFunction();
-	Type::param_iterator i=tmp->getFunctionType().param_begin();
-	string fname=tmp->getName().str();
-	//list<value> arglist=tmp->getArgumentList();
-	
-	if(fname.equals("lock.load"))
-	{
-	    //list<value*>::iterator i=arglist.begin();
-	    LoadInst* LI=new LoadInst(**(i));
-	    if(**(i+1)==true)
-		LI->setAtomic(**(i+4),**(i+5));
-	    LI->setVolatile(**(i+2));
-	    LI->setAlignment(**(i+3));
-	    I=LI;
-	}	    
-    }
+   string::size_type pos;
+   SmallVector<string, 10> result;
+   str+=pattern;
+   unsigned size=str.size();
+   for(unsigned i = 0; i < size; i++){
+      pos=str.find(pattern, i);
+      if(pos < size){
+         string s = str.substr(i,pos-i);
+         result.push_back(s);
+         i=pos+pattern.size()-1;
+      }
+   }
+   return result;
+}
+void Unlock::unlock_inst(Instruction* I)
+{
+   LLVMContext& C = I->getContext();
+   Module* M = I->getParent()->getParent()->getParent();
+   CallInst* CI=cast<CallInst>(I);
+   SmallVector<Type*, 8>OpTypes;
+   SmallVector<Value*, 8>OpArgs;
+   for(Instruction::op_iterator Op = I->op_begin(), E = I->op_end(); Op!=E; ++Op){
+      OpTypes.push_back(Op->get()->getType());
+      OpArgs.push_back(Op->get());
+   }
+   Function* F=CI->getCalledFunction();
+   string cname=F->getName().str();
+   SmallVector<pair<unsigned int, MDNode*>, 8> MDNodes;
+   CI->getAllMetadata(MDNodes);
+   SmallVector<StringRef, 30> names; 
+   C.getMDKindNames(names);
+   Instruction* T = NULL;
+   if(cname.find("lock.load") < cname.length()){
+      LoadInst* LI=new LoadInst(OpArgs[0],"",I);
+      SmallVector<string, 10> tmp;
+      for(unsigned i = 0; i < MDNodes.size(); i++){
+         // cerr<<names[MDNodes[i].first].str()<<endl;
+         tmp=str_split(names[MDNodes[i].first].str(),".");
+         //cerr<<tmp[0]<<"\t"<<endl;
+         if(tmp[0]=="volatile")
+            LI->setVolatile(true);
+         else if(tmp[0]=="atomic"){
+            LI->setAtomic((AtomicOrdering)(atoi(tmp[1].c_str())));
+         }
+         else if(tmp[0]=="align")
+            LI->setAlignment(atoi(tmp[1].c_str()));
+         else
+            LI->setMetadata(MDNodes[i].first, MDNodes[i].second);
+      }
+      I->replaceAllUsesWith(LI);
+      for(unsigned i =0;i < I->getNumOperands();i++)
+      {
+         I->setOperand(i, UndefValue::get(I->getOperand(i)->getType()));
+      }
+      //for(unsigned i = 0; i < MDNodes.size(); i++)
+        // MDNodes[i].second->replaceOperandWith(MDNodes[i].first,UndefValue::get(I->getOperand(i)->getType()));
+      I->removeFromParent();
+      //F->removeFromParent();
+      //cerr<<endl;
+      //cerr<<"found lock.\t"<<cname<<endl;
+      T=LI;
+   }
+   else
+      cerr<<"not found lock."<<endl;
+}
+/*
+   void Lock::unlock_inst(Instruction *I)
+   {
+   if (CallInst* CI=dyn_cast<CallInst>(I))
+   {
+   Function* tmp=CI->getCalledFunction();
+   Type::param_iterator i=tmp->getFunctionType().param_begin();
+   string fname=tmp->getName().str();
+//list<value> arglist=tmp->getArgumentList();
+
+if(fname.equals("lock.load"))
+{
+//list<value*>::iterator i=arglist.begin();
+LoadInst* LI=new LoadInst(**(i));
+if(**(i+1)==true)
+LI->setAtomic(**(i+4),**(i+5));
+LI->setVolatile(**(i+2));
+LI->setAlignment(**(i+3));
+I=LI;
+}	    
+}
 }
 */
